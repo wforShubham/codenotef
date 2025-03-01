2af764b26324363e6324aafa9501f932b1c2a61c
Shubham Vishwakarma <94854000+wforShubham@users.noreply.github.com>
2025-03-02 02:49:23 +0530

    Add files via upload

---
+++ src/components/Chome.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import codelogo from "./cnlg.png";
+
+function Chome() {
+  const [roomId, setRoomId] = useState("");
+  const [username, setUsername] = useState("");
+
+  const navigate = useNavigate();
+
+  const isLoggedIn = localStorage.getItem("token");
+    
+        if (!isLoggedIn) {
+          toast.error("Please log in to access this feature.");
+          navigate("/login");
+          return;
+        }
+
+  const generateRoomId = (e) => {
+    e.preventDefault();
+    const Id = uuid();
+    setRoomId(Id);
+    toast.success("Room Id is generated");
+  };
+
+  const joinRoom = () => {
+    if (!roomId || !username) {
+      toast.error("Both the field is requried");
+      return;
+    }
+
+    // redirect
+    navigate(`/editor/${roomId}`, {
+      state: {
+        username,
+      },
+    });
+    toast.success("room is created");
+  };
+
+  // when enter then also join
+  const handleInputEnter = (e) => {
+    if (e.code === "Enter") {
+      joinRoom();
+    }
+  };
+
+  return (
+    <div className="container-fluid">
+      <div className="row justify-content-center align-items-center min-vh-50">
+        <div className="col-12 col-md-6">
+          <div className="card shadow-sm p-2 mb-5 bg-secondary rounded">
+            <div className="card-body text-center bg-dark">
+              <img
+                src={codelogo}
+                alt="Logo"
+                className="img-fluid mx-auto d-block"
+                style={{ maxWidth: "150px" }}
+              />
+              <h4 className="card-title text-light mb-4">Enter the ROOM ID</h4>
+
+              <div className="form-group">
+                <input
+                  type="text"
+                  value={roomId}
+                  onChange={(e) => setRoomId(e.target.value)}
+                  className="form-control mb-2"
+                  placeholder="ROOM ID"
+                  onKeyUp={handleInputEnter}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="form-control mb-2"
+                  placeholder="USERNAME"
+                  onKeyUp={handleInputEnter}
+                />
+              </div>
+              <button
+                onClick={joinRoom}
+                className="btn btn-outline-success mx-1 rounded-4"
+              >
+                JOIN
+              </button>
+              <p className="mt-3 text-light">
+                Don't have a room ID? create{" "}
+                <span
+                  onClick={generateRoomId}
+                  className=" text-success p-2"
+                  style={{ cursor: "pointer" }}
+                >
+                  {" "}
+                  New Room
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Chome;
